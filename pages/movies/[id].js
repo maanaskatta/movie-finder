8e73c625f8ba9f751bb887839f4eca3db0680d6a
Maanas Katta <maanaskatta6@gmail.com>
2021-01-25 22:52:33 +0530

    Fixed a bug

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -36,7 +36,7 @@ const MovieDetails = ({ details }) => {
                 </a>
               </Link>
 
-              <Link href="/">
+              <Link href="/dashboard">
                 <a className="px-3 py-2 rounded-lg bg-yellow-600 text-white">
                   Back to Home
                 </a>
